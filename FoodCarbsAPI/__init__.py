ee41df691774cc327e4497a944eb9ab56b087ab5
Jaime Borges <jeborgesm@hotmail.com>
2025-03-21 18:42:29 -0500

    changes to logging

--- FoodCarbsAPI/__init__.py
+++ FoodCarbsAPI/__init__.py
@@ -39,14 +39,12 @@ def create_app(config_class='FoodCarbsAPI.config.Config'):
 
     # Configure logging
     if not app.debug and not app.testing:
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
         ))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('App startup')
@@ -80,4 +78,3 @@ def create_app(config_class='FoodCarbsAPI.config.Config'):
     got_request_exception.connect(log_exception, app)
 
     return app
-
