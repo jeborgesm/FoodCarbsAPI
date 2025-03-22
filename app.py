01c418d34cf0bac311fc2dcb6bafc1bfbd174c5e
Jaime Borges <jeborgesm@hotmail.com>
2025-03-21 19:01:54 -0500

    fix to logging

--- app.py
+++ app.py
@@ -6,19 +6,13 @@ app = create_app()
 # Set up logging
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
-# Handler for warning and above
-warning_handler = logging.FileHandler('logs/warning.log')
-warning_handler.setLevel(logging.WARNING)
-warning_handler.setFormatter(formatter)
-
-# Handler for info and above
-info_handler = logging.FileHandler('logs/info.log')
-info_handler.setLevel(logging.INFO)
-info_handler.setFormatter(formatter)
+# Console handler for warning and above
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.WARNING)
+console_handler.setFormatter(formatter)
 
 # Add handlers to the app logger
-app.logger.addHandler(warning_handler)
-app.logger.addHandler(info_handler)
+app.logger.addHandler(console_handler)
 
 if __name__ == '__main__':
     import os
